f230d8c743a08a5588134a470a690e039bb0e4ec
Mario Alberto Barragán Espinosa <>
2019-10-13 15:16:36 -0500

    Temperature converter

--- Challenge1/ContentView.swift
+++ Challenge1/ContentView.swift
@@ -9,8 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var inputIndex = 0
+    @State private var outputIndex = 1
+    @State private var inputValue = ""
+    
+    let temperatureUnits: [UnitTemperature] = [.celsius, .fahrenheit, .kelvin]
+    
+    var conversionResult: String {
+        guard let unitValue = Double(inputValue) else {
+            return "Invalid Input"
+        }
+        let value = Measurement(value: unitValue, unit: temperatureUnits[inputIndex])
+        let convertedValue = value.converted(to: temperatureUnits[outputIndex])
+        let formatted = String(format: "%.2f \(convertedValue.unit.symbol)", convertedValue.value)
+        return "\(formatted)"
+    }
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section(header: Text("Input Temperature")) {
+                    Picker("Choose your unit", selection: $inputIndex) {
+                        ForEach(0..<temperatureUnits.count) {
+                            Text("\(self.temperatureUnits[$0].symbol)")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Output Temperature")) {
+                    Picker("Choose your unit", selection: $outputIndex) {
+                        ForEach(0..<temperatureUnits.count) {
+                            Text("\(self.temperatureUnits[$0].symbol)")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Write your input")) {
+                    TextField("\(temperatureUnits[inputIndex].symbol)", text: $inputValue)
+                }
+                
+                Section(header: Text("Result")) {
+                    Text(conversionResult)
+                }
+            }
+            .navigationBarTitle("Unit conversions")
+        }
     }
 }
 
