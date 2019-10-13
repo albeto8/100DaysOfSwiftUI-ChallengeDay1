88a0e16c2b7567ed055597ad6f0f4b9bc9bcca88
Mario Alberto Barragán Espinosa <>
2019-10-13 12:43:43 -0500

    First commit

---
+++ Challenge1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge1
+//
+//  Created by Mario Alberto Barragán Espinosa on 13/10/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
